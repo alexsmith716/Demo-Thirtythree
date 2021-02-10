087bf9d43eac863f41c988258f0a0f2c328a645b
AlexSmith716 <1691290+alexsmith716@users.noreply.github.com>
2021-02-10 18:21:52 -0500

    fix: Cleaned-up and possibly fixed RESTfulExample queries and input works.

--- src/containers/RESTfulExample/RESTfulExample.js
+++ src/containers/RESTfulExample/RESTfulExample.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { 
-	useLazyQuery,
-	useApolloClient,
-	NetworkStatus,
+  useLazyQuery,
+  useApolloClient,
+  NetworkStatus,
 } from '@apollo/client';
 
 import Button from '../../components/Button';
@@ -13,244 +13,239 @@ import { GET_GOOGLE_BOOKS, GET_GOOGLE_BOOK } from '../../graphql/queries/queries
 
 const RESTfulExample = () => {
 
-	const [clientExtract, setClientExtract] = useState(null);
-	const [googleBookSearch, setGoogleBookSearch] = useState('');
+  const [clientExtract, setClientExtract] = useState(null);
+  const [googleBookSearch, setGoogleBookSearch] = useState('');
 
-	const client = useApolloClient();
+  const client = useApolloClient();
 
-	const [getGoogleBooks, {
-			loading, 
-			error,
-			data: googleBooksData,
-			previousData: googleBooksPreviousData,
-			refetch,
-			fetchMore,
-			networkStatus,
-		}] = useLazyQuery(
-			GET_GOOGLE_BOOKS,
-			{
-				variables: {
-					// searchString: `${googleBookSearch}`,
-					searchString: '',
-					orderBy: 'newest',
-				},
-				//  fetchPolicy: 'cache-and-network',
-				//  pollInterval: 500,
-				notifyOnNetworkStatusChange: true,
-			}
-	);
+  const [getGoogleBooks, {
+      loading, 
+      error,
+      data: googleBooksData,
+      previousData: googleBooksPreviousData,
+      refetch,
+      fetchMore,
+      networkStatus,
+    }] = useLazyQuery(
+      GET_GOOGLE_BOOKS,
+      {
+        variables: {
+          // searchString: `${googleBookSearch}`,
+          // searchString: '',
+          orderBy: 'newest',
+        },
+        fetchPolicy: 'cache-and-network',
+        nextFetchPolicy: 'cache-first',
+        notifyOnNetworkStatusChange: true,
+      }
+  );
 
-	const [getGoogleBook, {
-			loading: googleBookLoading, 
-			error: googleBookError,
-			data: googleBookData,
-		}] = useLazyQuery(
-			GET_GOOGLE_BOOK,
-	);
+  const [getGoogleBook, {
+      loading: googleBookLoading, 
+      error: googleBookError,
+      data: googleBookData,
+    }] = useLazyQuery(
+      GET_GOOGLE_BOOK,
+  );
 
-	// Provide a previousData property in useQuery/useLazyQuery results
-	// result.data ?? result.previousData to obtain the most recent useful data
-	useEffect(() => {
-			if (googleBooksData) {
-				console.log('>>>>>>>>>>>>>>>>>>>>>>>> RESTfulExample > useEffect() > googleBooksData: ', googleBooksData);
-				//console.log('>>>>>>>>>>>>>>>>>>>>>>>> RESTfulExample > useEffect() > googleBooksData.googleBooks.cursor: ', googleBooksData.googleBooks.cursor);
-			}
-			if (googleBookSearch) {
-				console.log('>>>>>>>>>>>>>>>>>>>>>>>> RESTfulExample > useEffect() > googleBookSearch: ', googleBookSearch);
-			}
-			if (googleBookData) {
-				console.log('>>>>>>>>>>>>>>>>>>>>>>>> RESTfulExample > useEffect() > googleBookData.googleBook: ', googleBookData.googleBook);
-			}
-		},
-		[googleBooksData, googleBookSearch, googleBookData]
-	);
+  useEffect(() => {
+      if (googleBooksData) {
+        console.log('>>>>>>>>>>>>>>>>>>>>>>>> RESTfulExample > useEffect() > googleBooksData: ', googleBooksData);
+        //console.log('>>>>>>>>>>>>>>>>>>>>>>>> RESTfulExample > useEffect() > googleBooksData.googleBooks.cursor: ', googleBooksData.googleBooks.cursor);
+      }
+      if (googleBookSearch) {
+        console.log('>>>>>>>>>>>>>>>>>>>>>>>> RESTfulExample > useEffect() > googleBookSearch: ', googleBookSearch);
+      }
+      if (googleBookData) {
+        console.log('>>>>>>>>>>>>>>>>>>>>>>>> RESTfulExample > useEffect() > googleBookData.googleBook: ', googleBookData.googleBook);
+      }
+    },
+    [googleBooksData, googleBookSearch, googleBookData]
+  );
 
-	return (
-		<>
-			<Helmet title="REST Example" />
+  return (
+    <>
+      <Helmet title="REST Example" />
 
-			{/* ---------------------------------------------- */}
+      {/* ---------------------------------------------- */}
 
-			<div className="container">
-				{/* ---------------------------------------------- */}
+      <div className="container">
+        {/* ---------------------------------------------- */}
 
-				<h1 className="mt-4 mb-3">REST Example</h1>
+        <h1 className="mt-4 mb-3">REST Example</h1>
 
-				{/* ---------------------------------------------- */}
+        {/* ---------------------------------------------- */}
 
-				<div className="bg-color-ivory container-padding-border-radius-1 text-break mb-5">
-					<div className="mb-3">
+        <div className="bg-color-ivory container-padding-border-radius-1 text-break mb-5">
+          <div className="mb-3">
 
-						{networkStatus === NetworkStatus.refetch && (
-							<p>
-								Refetching...
-							</p>
-						)}
+            {networkStatus === NetworkStatus.refetch && (
+              <p>
+                Refetching...
+              </p>
+            )}
 
-						{loading && (
-							<p>
-								Loading...
-							</p>
-						)}
+            {loading && (
+              <p>
+                Loading...
+              </p>
+            )}
 
-						{googleBookLoading && (
-							<p>
-								Loading...
-							</p>
-						)}
+            {googleBookLoading && (
+              <p>
+                Loading...
+              </p>
+            )}
 
-						{error && (
-							<p>
-								Query Error: {error.message}
-							</p>
-						)}
+            {error && (
+              <p>
+                Query Error: {error.message}
+              </p>
+            )}
 
-						{googleBookError && (
-							<p>
-								Query Error: {googleBookError.message}
-							</p>
-						)}
+            {googleBookError && (
+              <p>
+                Query Error: {googleBookError.message}
+              </p>
+            )}
 
-						{googleBooksData && (
-							<div>
-								<div className="mb-3">
-									<h5>getGoogleBooks Data:</h5>
-								</div>
-								{/* ----------------------------------------------------------------------- */}
-								{googleBooksData.googleBooks.books.map((book, index) => (
-									<div key={index} className="mb-3 container-padding-border-radius-2">
-										<GoogleBookBook book={ book } />
-									</div>
-								))}
-								{/* ----------------------------------------------------------------------- */}
-							</div>
-						)}
+            {googleBooksData && (
+              <div>
+                <div className="mb-3">
+                  <h5>getGoogleBooks Data:</h5>
+                </div>
+                {/* ----------------------------------------------------------------------- */}
+                {googleBooksData.googleBooks.books.map((book, index) => (
+                  <div key={index} className="mb-3 container-padding-border-radius-2">
+                    <GoogleBookBook book={ book } />
+                  </div>
+                ))}
+                {/* ----------------------------------------------------------------------- */}
+              </div>
+            )}
 
-						{googleBookData && (
-							<div>
-								<div className="mb-3">
-									<h5>getGoogleBook Data:</h5>
-								</div>
-								{/* ----------------------------------------------------------------------- */}
-									<div key={googleBookData.googleBook.id} className="mb-3 container-padding-border-radius-2">
-										<GoogleBookBook book={ googleBookData.googleBook } />
-									</div>
-								{/* ----------------------------------------------------------------------- */}
-							</div>
-						)}
+            {googleBookData && (
+              <div>
+                <div className="mb-3">
+                  <h5>getGoogleBook Data:</h5>
+                </div>
+                {/* ----------------------------------------------------------------------- */}
+                  <div key={googleBookData.googleBook.id} className="mb-3 container-padding-border-radius-2">
+                    <GoogleBookBook book={ googleBookData.googleBook } />
+                  </div>
+                {/* ----------------------------------------------------------------------- */}
+              </div>
+            )}
 
-						{clientExtract !== null && (
-							<div>
-								<h5>ApolloClient Cache:</h5>
-								<div>----------------------------------</div>
-								<div>{JSON.stringify(clientExtract)}</div>
-								<div>----------------------------------</div>
-							</div>
-						)}
-					</div>
+            {clientExtract !== null && (
+              <div>
+                <h5>ApolloClient Cache:</h5>
+                <div>----------------------------------</div>
+                <div>{JSON.stringify(clientExtract)}</div>
+                <div>----------------------------------</div>
+              </div>
+            )}
+          </div>
 
-					<div className="mb-3">
-						<Button
-							type="button"
-							className="btn-success btn-md"
-							onClick={() => setClientExtract(client.extract())}
-							buttonText="View Apollo Cache"
-						/>
-					</div>
+          <div className="mb-3">
+            <Button
+              type="button"
+              className="btn-success btn-md"
+              onClick={() => setClientExtract(client.extract())}
+              buttonText="View Apollo Cache"
+            />
+          </div>
 
-					<div className="mb-3">
-						<Button
-							type="button"
-							className="btn-success btn-md"
-							onClick={() => refetch()}
-							buttonText="RefetchQueryResults"
-						/>
-					</div>
+          <div className="mb-3">
+            <Button
+              type="button"
+              className="btn-success btn-md"
+              onClick={() => refetch()}
+              buttonText="RefetchQueryResults"
+            />
+          </div>
 
-					<div className="mb-3">
-						<Button
-							type="button"
-							className="btn-success btn-md"
-							onClick={ () => getGoogleBook({ variables: { id: 'uW_zzQEACAAJ' }}) }
-							buttonText="Get Book ID: uW_zzQEACAAJ"
-						/>
-					</div>
+          <div className="mb-3">
+            <Button
+              type="button"
+              className="btn-success btn-md"
+              onClick={ () => getGoogleBook({ variables: { id: 'uW_zzQEACAAJ' }}) }
+              buttonText="Get Book ID: uW_zzQEACAAJ"
+            />
+          </div>
 
-					<div className="mb-3">
-						<Button
-							type="button"
-							className="btn-success btn-md"
-							//onClick={() => setGoogleBookSearch('mcat')}
-							onClick={() => getGoogleBooks({ variables: { searchString: 'kaplan test prep' }, fetchPolicy: 'network-only'})}
-							buttonText="Search Kaplan"
-						/>
-					</div>
+          <div className="mb-3">
+            <Button
+              type="button"
+              className="btn-success btn-md"
+              onClick={() => getGoogleBooks({ variables: { searchString: 'kaplan test prep' },})}
+              buttonText="Search Kaplan"
+            />
+          </div>
 
-					<div className="mb-3">
-						<Button
-							type="button"
-							className="btn-success btn-md"
-							//onClick={() => setGoogleBookSearch('gmat')}
-							onClick={() => getGoogleBooks({ variables: { searchString: 'gmat' }, fetchPolicy: 'network-only'})}
-							buttonText="Search GMAT"
-						/>
-					</div>
+          <div className="mb-3">
+            <Button
+              type="button"
+              className="btn-success btn-md"
+              onClick={() => getGoogleBooks({ variables: { searchString: 'gmat' },})}
+              buttonText="Search GMAT"
+            />
+          </div>
 
-					<div className="mb-3">
-						<Button
-							type="button"
-							className="btn-success btn-md"
-							//onClick={() => setGoogleBookSearch('lsat')}
-							onClick={() => getGoogleBooks({ variables: { searchString: 'lsat' }, fetchPolicy: 'network-only'})}
-							buttonText="Search LSAT"
-						/>
-					</div>
+          <div className="mb-3">
+            <Button
+              type="button"
+              className="btn-success btn-md"
+              onClick={() => getGoogleBooks({ variables: { searchString: 'lsat' },})}
+              buttonText="Search LSAT"
+            />
+          </div>
 
-					<div className="mb-3">
-						<div className="row-flex">
-							<div className="col-four">
-								<input
-									type="text"
-									className="form-control"
-									name="googleBookSearch"
-									value={googleBookSearch}
-									onChange={e => setGoogleBookSearch(e.target.value)}
-									placeholder="Search Google Books"
-								/>
-							</div>
-						</div>
-					</div>
+          <div className="mb-3">
+            <div className="row-flex">
+              <div className="col-four">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="googleBookSearch"
+                  value={googleBookSearch}
+                  onChange={e => setGoogleBookSearch(e.target.value)}
+                  placeholder="Search Google Books"
+                />
+              </div>
+            </div>
+          </div>
 
-					<div className="mb-3">
-						<Button
-							type="button"
-							className="btn-success btn-md"
-							onClick={() => getGoogleBooks()}
-							buttonText="Get Google Books"
-						/>
-					</div>
+          <div className="mb-3">
+            <Button
+              type="button"
+              className="btn-success btn-md"
+              onClick={() => getGoogleBooks({ variables: { searchString: googleBookSearch },})}
+              buttonText="Get Google Books"
+            />
+          </div>
 
-					{googleBooksData && (
-						<div className="mb-3">
-							<Button
-								type="button"
-								className="btn-primary btn-md"
-								onClick={ async () => {
-									await fetchMore({
-										variables: {
-											after: googleBooksData.googleBooks.cursor,
-										},
-									});
-								}}
-								buttonText="fetchMore Google Books"
-							/>
-						</div>
-					)}
+          {googleBooksData && (
+            <div className="mb-3">
+              <Button
+                type="button"
+                className="btn-primary btn-md"
+                onClick={ async () => {
+                  await fetchMore({
+                    variables: {
+                      after: googleBooksData.googleBooks.cursor,
+                    },
+                  });
+                }}
+                buttonText="fetchMore Google Books"
+              />
+            </div>
+          )}
 
-				</div>
-			</div>
-		</>
-	);
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default RESTfulExample;
